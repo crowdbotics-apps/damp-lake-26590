e5c1c3a07783249ef8fb7d3ec60ad7afa9b7f9f8
Crowdbotics <team@crowdbotics.com>
2021-05-12 04:37:11 +0000

    Initial commit.

---
+++ backend/damp_lake_26590/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for damp_lake_26590 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'damp_lake_26590.settings')
+
+application = get_wsgi_application()
